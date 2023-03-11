619039443829cd11902b7371ab8a8146114ca32e
carla bruna <carlabrunacappelluppi@gmail.com>
2023-03-11 16:39:45 -0300

    modificação calaculadora e media

--- js/media.js
+++ js/media.js
@@ -14,13 +14,28 @@
 
 function calcularMedia() {
   const nota1 = parseFloat(document.getElementById("nota1").value);
-  const note2 = parseFloat(document.getElementById("nota2").value);
-  const note3 = parseFloat(document.getElementById("nota3").value);
-  const note4 = parseFloat(document.getElementById("nota4").value);
+  const nota2 = parseFloat(document.getElementById("nota2").value);
+  const nota3 = parseFloat(document.getElementById("nota3").value);
+  const nota4 = parseFloat(document.getElementById("nota4").value);
 
   let mensagem = "";
   // evite mexer no código acima!
-  
+
+  let media = (nota1 + nota2 + nota3 + nota4) / 4
+   if (media === 0){
+    mensagem = 'Infelizmente você zerou a prova'; 
+   } else if (media <= 3){
+    mensagem = 'Você obteve media! Falta pouco para a média';
+   }else if (media <= 5){
+    mensagem = 'Você obteve media! Falta pouco para a média';
+  }else if (media <= 7){
+    mensagem = 'nota';
+  }else if(media <= 9){
+    mensagem = '"Notão! Sua média é';
+  }else if(media <= 10){
+    mensagem = 'Hoje é seu aniversário? Pq você ta de parabéns!';
+  }
+
 
   
   
